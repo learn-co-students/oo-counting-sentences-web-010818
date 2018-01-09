035d9e34401aa90cdc7da93d852f8e20a4202801
cnlucke <cristy.lucke@gmail.com>
2018-01-09 18:00:10 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[self.size - 1] == "."
   end
 
   def question?
-
+    self[self.size - 1] == "?"
   end
 
   def exclamation?
-
+    self[self.size - 1] == "!"
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count # scan has regex to split string and strip will remove trailing spaces.
   end
 end
