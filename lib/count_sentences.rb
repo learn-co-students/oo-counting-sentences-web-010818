9ecc986cc2f040c128f0709b1779f41a402702f3
cnlucke <cristy.lucke@gmail.com>
2018-01-10 08:39:50 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,20 @@ class String
   end
 
   def count_sentences
-    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count # scan has regex to split string and strip will remove trailing spaces.
+    # scan has regex to split string and strip will remove trailing spaces.
+    # credit: https://stackoverflow.com/questions/32451429/count-number-of-sentence-ruby
+    # self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
+
+    # More naive solution
+    words = self.split
+    #find all words that end with ".!?"
+    count = 0
+    words.each do |word|
+      last = word[word.size - 1]
+      if (last == ".") || (last == "?") || (last == "!")
+        count += 1
+      end
+    end
+    count
   end
 end
