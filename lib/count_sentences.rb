8f8b2f84fc9bdbce0f4df1ffe4a2c9e1935281a8
Vaibhav Patel <vp932712@gmail.com>
2018-01-11 18:26:04 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,29 @@ require 'pry'
 
 class String
 
+  # attr_accessor :str
+  #
+  # def initialize(str)
+  #   @str = str
+  # end
+
   def sentence?
-    
+    self.end_with?(".")
+
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    self.squeeze('.!?').count('.!?')
+    
 
   end
 end
